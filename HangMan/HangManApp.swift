7fdefc0c4552e37496192ded5134ab9ae52c63e1
Preston Fitzgerald <pgf23@byu.edu>
2024-12-16 08:58:05 -0700

    Initial Commit

---
+++ HangMan/HangManApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangManApp.swift
+//  HangMan
+//
+//  Created by IS 543 on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HangManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
